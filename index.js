06c8a4a6817652ad34b816196f2e1a07404fdf93
Johan Persson <johan.persson91@hotmail.com>
2023-09-28 10:07:57 +0200

    Log in verkar funka nu

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-const logInForm = document.getElementById("loginForm");
-const successMessage = document.getElementById("successLogIn");
+
+let logInForm = document.getElementById("loginForm");
+    let successMessage = document.getElementById("successLogIn");
+
+
 
 function logIn(){
+   
+   
     const namn = "Bella";
     const lösenord = "qwe123";
+       
+    let username = logInForm.name.value;
+    let userPassword = logInForm.password.value;
 
-    const username = logInForm.name.value;
-    const userPassword = logInForm.password.value;
-    
-    if(username === namn && (userPassword === lösenord)){
+    if(username == namn && userPassword == lösenord){
+        localStorage.setItem('username', logInForm.name.value);
+        localStorage.setItem('password', logInForm.password.value);
+        logInForm.style.visibility = 'hidden';
+        successMessage.style.display = 'flex';
         
-        logInForm.style.visibility = "hidden";
-        successMessage.style.display = "flex";
+    }else{
+        alert("Failure");
     }
+
 }
 
 function returnToLoginForm(){
-    logInForm.style.visibility = "visible";
-    successMessage.style.visibility = "none";
+    localStorage.clear();
+    logInForm.style.visibility = 'visible';
+    successMessage.style.display = 'none';
 }
+
+function checkLogin(){
+    let user = localStorage.getItem('username');
+    let pass = localStorage.getItem('password');    
+
+    if(user =="Bella"){
+        logInForm.style.visibility = 'hidden';
+        successMessage.style.display = 'flex';
+    }
+    
+}
+
+window.onload = checkLogin;
